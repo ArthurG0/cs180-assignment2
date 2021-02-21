c4009419134da3a365c4f00e601b1c6c7029007c
ArthurG0 <agatin@umail.ucsb.edu>
2021-02-21 01:37:28 -0800

    end of day dynamic tabs

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -97,7 +97,7 @@ class Tab extends React.Component {
       onMouseLeave={this.onMouseExit}
       onClick={this.onIWasPressed}
       >
-        {this.props.id === this.props.selectedId ? "1" : "2"}{this.state.beingHovered ? " 3": " 4"}
+        {this.props.id}{this.props.id === this.props.selectedId ? " a" : " n a"}{this.state.beingHovered ? " h": " n h"}
       </div>
     );
   }
